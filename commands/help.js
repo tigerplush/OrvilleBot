f1e9e6dbd6c0c0badb0e7f2cad86c477390441d1
Tig0r <christopher.putz@sheep-records.de>
2020-04-21 23:57:35 +0200

    Improved help and formatting

--- commands/help.js
+++ commands/help.js
@@ -9,17 +9,21 @@ module.exports =
         const {commands} = message.client;
         
         let answer = "";
-        answer += prefix;
+        answer += "`" + prefix;
         if(!args.length)
         {
-            answer += this.name + "\n" + this.description + "\nAvailable commands: " + commands.map(command => command.name).join(', ') + "\nGet more info with !help <command>";
+            answer += this.name + "`\n" + this.description + "\n";
+            answer += "Available commands: `" + prefix + commands.map(command => command.name).join('`, `' + prefix) + "`\n";
+            answer += "To open your island, use `!open <dodo_code>` (with your dodo code inserted)\n";
+            answer += "To close your island, just type `!close`\n";
+            answer += "Get more info with `!help <command>`";
         }
         else
         {
             if(commands.has(args[0]))
             {
                 command = commands.get(args[0])
-                answer += command.name + " " + command.usage + "\n" + command.description;
+                answer += command.name + "` " + command.usage + "\n" + command.description;
             }
             else
             {
