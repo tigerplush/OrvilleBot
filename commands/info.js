c435a0eb10fd849eb10803ca1ad3bad1795c84a0
Tig0r <christopher.putz@sheep-records.de>
2020-04-22 14:31:02 +0200

    Added github to info command

--- commands/info.js
+++ commands/info.js
@@ -9,7 +9,8 @@ module.exports =
         let infoString = "";
         infoString += package.name + " v" + package.version + "\n";
         infoString += package.description + "\n";
-        infoString += "by " + package.author;
+        infoString += "by " + package.author + "\n";
+        infoString += "If you want to contribute, report bugs or suggest features and enhancements, please go to " + package.github;
         message.channel.send(infoString);
     },
 };
