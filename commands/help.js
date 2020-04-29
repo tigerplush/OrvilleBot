f89a6ce1146f7f22cc0358b38e331b083bc8814d
Tig0r <christopher.putz@sheep-records.de>
2020-04-29 12:20:14 +0200

    updated help to contain prefix

--- commands/help.js
+++ commands/help.js
@@ -24,7 +24,7 @@ module.exports =
             answer += "\n";
             answer += "To open your island, use `" + prefix + commands.get("open").name + " dodo code` (with your dodo code inserted)\n";
             answer += "To close your island, just type `" + prefix + commands.get("close").name + "`\n";
-            answer += "Get more info with `!help command`";
+            answer += "Get more info with `" + prefix + commands.get("help").name +" command`";
         }
         else
         {
