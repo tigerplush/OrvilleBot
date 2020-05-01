c4f7d84df60f68f6ea37ba47556f1083836b83e7
Tig0r <christopher.putz@sheep-records.de>
2020-05-01 11:06:49 +0200

    added more debugging output for airport server id

--- database.js
+++ database.js
@@ -26,7 +26,7 @@ module.exports =
                 }
                 else
                 {
-                    reject("No airport found");
+                    reject("No airport for server + " + serverid + " found");
                 }
             });
         });
