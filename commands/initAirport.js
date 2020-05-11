45e4327d67bf4dcc47af4ed17bcd455cbbdcc1e2
Tig0r <christopher.putz@sheep-records.de>
2020-05-11 16:06:54 +0200

    fixed and simplified init-airport

--- commands/initAirport.js
+++ commands/initAirport.js
@@ -1,5 +1,13 @@
 const {airportsDb} = require('../Database/databases.js');
 
+class AirportError extends Error
+{
+    constructor(message)
+    {
+        super(message);
+    }
+}
+
 module.exports =
 {
     name: "init-airport",
@@ -21,35 +29,38 @@ module.exports =
         let newAirport = {};
         newAirport.serverid = serverid;
         newAirport.channelid = channelid;
-        airportsDb.getAirport(serverid)
-        .then(airport =>
+
+        airportsDb.get({serverid: serverid})
+        .then(airports =>
             {
-                let openMessage = "Airport successfully opened";
-                if(airport)
+                let verb = "opened";
+                if(airports && airports > 0)
                 {
-                    //there is an airport, update
-                    openMessage = "Airport successfully updated";
+                    const airport = airports[0];
+                    verb = "moved";
+                    if(airport.channelid === message.channel.id)
+                    {
+                        verb = "updated";
+                    }
                 }
-                openMessage += "\nReact within 1 minute with an emoji to set a reaction for queuing up users";
-                //create new airport
+
+                let openMessage = `Airport successfully ${verb}\nReact within 1 minute with an emoji to set a reaction for queuing up users`;
                 return message.channel.send(openMessage);
             })
-        .catch(err => console.log(err))
         .then(airportMessage =>
             {
                 openingMessage = airportMessage;
                 const filter = mes => mes;
-                airportMessage.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
-                .then(messageReactions =>
-                    {
-                        newAirport.emoji = messageReactions.first().emoji.name;
-                    })
-                .catch(err => console.log(err))
-                .finally(() =>
-                {
-                    openingMessage.edit("Airport is currently open!");
-                    message.client.emit('updateAirports', newAirport);
-                });
+                return airportMessage.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] });
+            })
+        .then(messageReactions =>
+            {
+                newAirport.emoji = messageReactions.first().emoji.name;
+            })
+        .then(() =>
+            {
+                openingMessage.edit("Airport is currently open!");
+                message.client.emit('updateAirports', newAirport);
             })
         .catch(err => console.log(err));
     },
